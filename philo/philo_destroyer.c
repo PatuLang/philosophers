dcb60fd86368ba3eb855e1298c4ec8c7f00ea1b1
PatuLang <patrik.laang@gmail.com>
2024-07-04 20:10:07 +0300

    adjustments in fork distribution stoped the stealing of forks, needs more testing and then we move on with the monitor

--- philo/philo_destroyer.c
+++ philo/philo_destroyer.c
@@ -6,7 +6,7 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 17:09:01 by plang             #+#    #+#             */
-/*   Updated: 2024/07/03 12:24:00 by plang            ###   ########.fr       */
+/*   Updated: 2024/07/04 15:51:00 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ int init_mutex_error(char *str, int i, t_philo *philo)
     {
         while (i > 0)
         {
-            if (&philo[i].right_fork)
+            // if (&philo[i].right_fork)
                 pthread_mutex_destroy(&philo[i].right_fork);
-            if (&philo[i].eating)
+            // if (&philo[i].eating)
                 pthread_mutex_destroy(&philo[i].eating);
             i--;
         }
     }
-    if (&philo->data->write_lock)
+    // if (&philo->data->write_lock)
         pthread_mutex_destroy(&philo->data->write_lock);
-    if (&philo->data->dead_lock)
+    // if (&philo->data->dead_lock)
         pthread_mutex_destroy(&philo->data->dead_lock);
     return (1);
 }
@@ -48,9 +48,9 @@ int data_mutex_error(char *str, t_data *data)
     // }
     // write(2, "\n", 1);
     printf("%s\n", str);
-    if (&data->write_lock)
+    // if (&data->write_lock)
         pthread_mutex_destroy(&data->write_lock);
-    if (&data->dead_lock)
+    // if (&data->dead_lock)
         pthread_mutex_destroy(&data->dead_lock);
     return (1);
 }
--- philo/philo_routine.c
+++ philo/philo_routine.c
@@ -6,51 +6,50 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 18:52:08 by plang             #+#    #+#             */
-/*   Updated: 2024/07/03 20:14:15 by plang            ###   ########.fr       */
+/*   Updated: 2024/07/04 20:00:12 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    fork_distribution(t_philo *philo)
-{
-    if (philo->id % 2 == 0)
-    {
-        pthread_mutex_lock(&philo->right_fork);
-        philo_actions(philo, "has taken a r_fork\n");
-    }
-    pthread_mutex_lock(philo->left_fork);
-    philo_actions(philo, "has taken a l_fork\n");
-    if (philo->id % 2 != 0)
-    {
-        // usleep(2000);
-        pthread_mutex_lock(&philo->right_fork);
-        philo_actions(philo, "has taken a r_fork\n");
-    }
-}
-
 void philo_dead_is(t_philo *philo)
 {
     size_t  time;
 
-    time = get_current_time() - philo->last_meal;
     pthread_mutex_lock(&philo->eating);
-    printf("philo: %d time: %zu last_meal: %zu\n",philo->id, time, philo->last_meal);
-    // usleep(20000);
+    time = get_current_time() - philo->last_meal;
+    // printf("philo: %d time: %zu last_meal: %zu\n",philo->id, time, philo->last_meal);
     if (time >= philo->data->time_to_die)
     {
         pthread_mutex_lock(&philo->data->dead_lock);
         philo->data->murder = 1;
-        philo_actions(philo, "choked on the spagetti for sure!\n");
+        // philo_actions(philo, "choked on the spagetti for sure!\n");
         pthread_mutex_unlock(&philo->data->dead_lock);
         pthread_mutex_unlock(&philo->eating);
     }
     pthread_mutex_unlock(&philo->eating);
 }
 
+void    fork_distribution(t_philo *philo)
+{
+    // if (philo->id % 2 == 0)
+    // {
+        pthread_mutex_lock(&philo->right_fork);
+        philo_actions(philo, "has taken a r_fork\n");
+    // }
+    pthread_mutex_lock(philo->left_fork);
+    philo_actions(philo, "has taken a l_fork\n");
+    // if (philo->id % 2 != 0)
+    // {
+    //     pthread_mutex_lock(&philo->right_fork);
+    //     philo_actions(philo, "has taken a r_fork\n");
+    // }
+    philo_dead_is(philo);
+}
+
 void    eat(t_philo *philo)
 {
-    philo_dead_is(philo);
+    // philo_dead_is(philo);
     fork_distribution(philo);
     pthread_mutex_lock(&philo->eating);
     // pthread_mutex_lock(&philo->data->write_lock);
@@ -61,10 +60,10 @@ void    eat(t_philo *philo)
     //     // printf("Philo number %d has died\n", philo->id);
     // }
     philo->last_meal = get_current_time();
-    ft_usleep(philo->data->time_to_eat);
-    philo_actions(philo, "is eating\n");
-    // printf("updated mealtime for philo:%i to :%zu\n", philo->id, philo->last_meal);
     pthread_mutex_unlock(&philo->eating);
+    philo_actions(philo, "is eating\n");
+    ft_usleep(philo->data->time_to_eat);
+    // printf("updated mealtime for philo:%i to :%zu\n", philo->id, philo->last_meal);
     philo->meals_eaten++;
     if (philo->id % 2 == 0)
         pthread_mutex_unlock(&philo->right_fork);
@@ -80,6 +79,7 @@ int    sleep_n_think(t_philo *philo)
     ft_usleep(philo->data->time_to_sleep);
     philo_dead_is(philo);
     philo_actions(philo, "is thinking\n");
+    philo_dead_is(philo);
     return (0);
 }
 
@@ -88,8 +88,8 @@ void    *philo_routine(void* arg)
     t_philo *philo;
 
     philo = (t_philo *)arg;
-    // if (philo->id % 2 == 0)
-    //     ft_usleep(philo->data->time_to_eat);
+    if (philo->id % 2 == 0)
+        ft_usleep(philo->data->time_to_eat - 10);
     while (1) // philo check murder function, while(!philo_murder_check)
     {
         eat(philo);
